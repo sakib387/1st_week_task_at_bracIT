3b3cc4cad2530f1486cde9e75e93569f66006743
Sakib Mollah <sakib.mollah@bracits.com>
2024-12-25 22:29:06 +0600

    make library class as a singleton so only one instance of library will be created

--- LibraryInventory/src/Library.java
+++ LibraryInventory/src/Library.java
@@ -1,2 +1,15 @@
+import java.util.List;
+
 public class Library {
+    private static  Library instance=null;
+    private List<Book> books;
+    private Library(){
+
+    }
+    public  static Library getLibraryInstance(){
+        if(instance==null){
+            instance=new Library();
+        }
+        return instance;
+    }
 }
